6113f7c7b750f415ee0eec719a13c841f3d3e81e
zakzakst <e1433104@gmail.com>
2023-03-19 17:53:02 +0900

    5-6

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,17 +1,31 @@
-export default async (chapterSlug: string, lessonSlug: string) => {
-  const { data, error } = await useFetch(
-    `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
-  );
-  // const { data, error } = await useAsyncData(() =>
-  //   $fetch(`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`)
-  // );
+import { StorageSerializers } from "@vueuse/core";
+import { LessonWithPath } from "~~/types/course";
 
-  if (error.value) {
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
-    });
+export default async (chapterSlug: string, lessonSlug: string) => {
+  const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`;
+  const lesson = useSessionStorage<LessonWithPath>(url, null, {
+    serializer: StorageSerializers.object,
+  });
+
+  if (!lesson.value) {
+    const { data, error } = await useFetch<LessonWithPath>(url);
+    // const { data, error } = await useAsyncData(() =>
+    //   $fetch(`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`)
+    // );
+
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug}`,
+      });
+    }
+
+    lesson.value = data.value;
+  } else {
+    console.log(
+      `Getting lesson ${lessonSlug} in chapter ${chapterSlug} from cache`
+    );
   }
 
-  return data;
+  return lesson;
 };
