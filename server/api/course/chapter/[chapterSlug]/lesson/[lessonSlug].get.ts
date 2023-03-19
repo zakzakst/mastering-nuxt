6c03fdce1e63c33429c519f8de4029863fdcb31e
zakzakst <e1433104@gmail.com>
2023-03-19 13:42:57 +0900

    5-3

--- server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -1,9 +1,12 @@
+import { Lesson, LessonWithPath, Chapter, Course } from "~/types/course";
 import course from "~~/server/courseData";
 
-export default defineEventHandler((event) => {
+course as Course;
+
+export default defineEventHandler((event): LessonWithPath => {
   const { chapterSlug, lessonSlug } = event.context.params;
 
-  const chapter = course.chapters.find(
+  const chapter: Maybe<Chapter> = course.chapters.find(
     (chapter) => chapter.slug === chapterSlug
   );
 
@@ -14,7 +17,9 @@ export default defineEventHandler((event) => {
     });
   }
 
-  const lesson = chapter.lessons.find((lesson) => lesson.slug === lessonSlug);
+  const lesson: Maybe<Lesson> = chapter.lessons.find(
+    (lesson) => lesson.slug === lessonSlug
+  );
 
   if (!lesson) {
     throw createError({
@@ -23,5 +28,8 @@ export default defineEventHandler((event) => {
     });
   }
 
-  return lesson;
+  return {
+    ...lesson,
+    path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+  };
 });
