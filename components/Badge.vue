14b682a64fa10e4e306ae3985738976c8c3f9025
zakzakst <e1433104@gmail.com>
2023-03-20 22:28:56 +0900

    7-1

---
+++ components/Badge.vue
@@ -0,0 +1,19 @@
+<template>
+  <span
+    class="-left-12 text-xl w-8 h-8 text-white rounded-full inline-flex absolute items-center justify-center mr-4"
+    :class="`${color}`"
+  >
+    <slot>&#10003;</slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    color?: string;
+  }>(),
+  {
+    color: "bg-emerald-400",
+  }
+);
+</script>
