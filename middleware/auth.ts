a96f2c14cea159834bc97881871ff0cd5ac5f3ff
zakzakst <e1433104@gmail.com>
2023-03-24 07:21:14 +0900

    7-15

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,14 @@
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const user = useSupabaseUser();
-  if (user.value || to.params.chapterSlug === "1-chapter-1") {
+
+  const { data: hasAccess } = await userFetch("/api/user/hasAccess", {
+    headers: useRequestHeaders(["cookies"]),
+  });
+  if (hasAccess.value || to.params.chapterSlug === "1-chapter-1") {
     return;
+  } else if (user.value && !hasAccess.value) {
+    const client = useSupabaseClient();
+    await client.auth.signOut();
   }
   return navigateTo(`/login?redirectTo=${to.path}`);
 });
--- server/utils/protectRoute.ts
+++ server/utils/protectRoute.ts
@@ -1,7 +1,20 @@
 import { H3Event } from "h3";
 
-export default (event: H3Event) => {
+export default async (event: H3Event) => {
   if (!event.context.user) {
+    throw createError({
+      statusCode: 401,
+      message: "Unauthorized",
+    });
+  }
+
+  const hasAccess = await $fetch("/api/user/hasAccess", {
+    headers: {
+      cookie: getHeader(event, "cookie"),
+    },
+  });
+
+  if (!hasAccess) {
     throw createError({
       statusCode: 401,
       message: "Unauthorized",
